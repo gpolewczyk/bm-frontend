55ba6b8cfe340e88e48fc0fba751127a2abad54e
Gniewomir Polewczyk <gniewomir.polewczyk@iteo.com>
2020-12-30 15:13:08 +0100

    fix sort utility

--- src/utils/sort/index.tsx
+++ src/utils/sort/index.tsx
@@ -2,7 +2,7 @@ import { BookmarkModel } from 'src/store/bookmarks/bookmarks.interfaces';
 
 export const sortByLabel = (array: BookmarkModel[]): BookmarkModel[] => {
   return array.sort((a, b) => {
-    if (a.label > b.label) return 1;
-    if (a.label < b.label) return -1;
+    if (a.label.toLowerCase() < b.label.toLowerCase()) return -1;
+    if (a.label.toLowerCase() > b.label.toLowerCase()) return 1;
   });
 };
